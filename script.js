42856ff0ea73a54f34c7925d32364f1d5494efaf
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-02 15:45:17 -0700

    Create function to change the color of the cells

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const sketchGrid = document.querySelector('#sketch-grid');
 
+
 function setGridSize(size) {
   //set sketchGrid so it has the correct number of rows and columns
   sketchGrid.style.gridTemplate = 'repeat(' + size + ', 1fr) / repeat(' + size + ', 1fr)';
-  
+
   for (let i = 0; i < (size * size); i++) {
     const cell = document.createElement('div');
     cell.setAttribute('class', 'cell');
@@ -11,10 +12,15 @@ function setGridSize(size) {
   }
 };
 
+function changeColor(mouse) {
+  const cell = mouse.target.classList;
+  cell.add('black');
+}
+
 
 setGridSize(16);
 const gridCells = document.querySelectorAll('.cell');
 
 gridCells.forEach(cell => {
-  cell.addEventListener('mouseover', (cell) => console.log(cell));
+  cell.addEventListener('mouseover', (mouseoverCell) => changeColor(mouseoverCell));
 });
